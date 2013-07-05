9582bafa4bf5633b15c4fb0d1cb2892388e39c00
Alexander Graebe <alex.graebe@gmail.com>
2013-07-05 23:54:31 +0000

    Added URL loading to grader

--- grader.js
+++ grader.js
@@ -28,7 +28,6 @@ var request = require('request');
 
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-var URL_DEFAULT = "http://secret-forest-4822.herokuapp.com/";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -38,20 +37,6 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
-
-var assertURLExists = function(infile) {
-    var instr = infile.toString();
-    var request = new XMLHttpRequest();  
-    request.open('GET', instr, true);  
-    request.send();  
-
-    if (request.status === "404") {  
-        console.log("%s does not exist. Exiting.", instr);
-	process.exit(1);
-    } else {
-	return instr;
-    }
-}
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -72,17 +57,25 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
-var checkURL = function(url, checksfile) {
-  request(url, function(err, resp, body){
-  $ = cheerio.load(body);
-     var checks = loadChecks(checksfile).sort();
-     var out = {};
-     for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
-     }
-     return out;
- });   
+var checkURLFile = function(html, checksfile) {
+    $ = cheerio.load(html);
+    var checks = loadChecks(checksfile).sort();                                                                                                                                 
+    var out = {};                                                                                                                                                               
+    for(var ii in checks) {                                                                                                                                                      
+        var present = $(checks[ii]).length > 0;                                                                                                                                 
+        out[checks[ii]] = present;                                                                                                                                              
+    }                                                                                                                                                                           
+    
+    console.log(JSON.stringify(out, null, 4));
+};                                                                                                                                                                              
+
+var getURL = function(url, checksfile) {
+    request(url, function (error, response, body) {
+    if (!error)
+        checkURLFile(body, checksfile);
+    else
+        console.log(error);
+    });
 };
 
 var clone = function(fn) {
@@ -95,15 +88,16 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .option('-u, --url <url_to_html_file>)', 'URL to index.html', clone(assertURLExists), URL_DEFAULT)
+        .option('-u, --url <url_to_html_file>)', 'URL to index.html')
 	.parse(process.argv);
-    if(program.file) {
+
+    if(program.url) {
+	getURL(program.url, program.checks);
+    } else {
 	var checkJson = checkHtmlFile(program.file, program.checks);
-    } else if(program.url) {
-	var checkJson = checkURL(program.url, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
     }
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
